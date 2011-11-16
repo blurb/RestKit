35f34498bb84a699e63cc0a4b3dbb3b5b7d583f9
Brian Williams <bwilliams@blurb.com>
2011-11-16 09:22:46 -0800

    fix three compiler warnings

--- Code/ObjectMapping/RKDynamicRouter.m
+++ Code/ObjectMapping/RKDynamicRouter.m
@@ -80,11 +80,11 @@
 	NSDictionary* classRoutes = [_routes objectForKey:className];
 	
 	NSString* resourcePath = nil;
-	if (resourcePath = [classRoutes objectForKey:methodName]) {
+	if ((resourcePath = [classRoutes objectForKey:methodName])) {
 		return RKMakePathWithObject(resourcePath, object);
 	}
 	
-	if (resourcePath = [classRoutes objectForKey:@"ANY"]) {
+	if ((resourcePath = [classRoutes objectForKey:@"ANY"])) {
 		return RKMakePathWithObject(resourcePath, object);
 	}
 	
